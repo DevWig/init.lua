95aa30d26c59bbc13c386cd0c705b37a6065b223
Hunter Wiginton <austin.wiginton@sap.com>
2023-10-27 11:01:08 -0500

    Move lualine plugin and configuration into dedicated plugin file and update theming

--- init.lua
+++ init.lua
@@ -139,22 +139,6 @@ require('lazy').setup({
 
     end,
   },
-
-  {
-    -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = false,
-        theme = 'onedark',
-        path = 1,
-        component_separators = '|',
-        section_separators = '',
-      },
-    },
-  },
-
   {
     -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
